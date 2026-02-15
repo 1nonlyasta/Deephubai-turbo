9ac1635dcea7120473d123406071998675511663
DeepHubAI Team <deephubai@hackathon.dev>
2026-02-15 12:55:54 -0800

    chore(cleanup): Remove legacy product components and unused assets

--- src/components/product/ProductAspectRatio.tsx
+++
@@ -1,9 +0,0 @@
-import * as AspectRatioPrimitive from "@radix-ui/react-aspect-ratio";
-
-/**
- * ProductAspectRatio component to maintain consistent dimensions 
- * for media assets (images, videos, etc).
- */
-const ProductAspectRatio = AspectRatioPrimitive.Root;
-
-export { ProductAspectRatio as AspectRatio };
